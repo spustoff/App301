f09f8a80738fc89ce4cc8bc3840ec5374de9d52e
spustoff <vyacheslavpustov05@gmail.com>
2023-12-24 13:04:11 +0300

    Initial Commit

---
+++ App301/App301App.swift
@@ -0,0 +1,17 @@
+//
+//  App301App.swift
+//  App301
+//
+//  Created by Вячеслав on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct App301App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App301/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App301
+//
+//  Created by Вячеслав on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
